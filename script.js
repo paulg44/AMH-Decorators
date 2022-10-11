9c82948dacb46683ecf1f6d0ecb17111ee95910c
Paul Garton <paulgarton1984@hotmail.com>
2022-10-11 21:04:52 +0100

    add java for panels, few other small changes

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach(panel => {
+  panel.addEventListener('click', () => {
+    removeActiveClasses();
+    panel.classList.add('active');
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach(panel => {
+    panel.classList.remove('active');
+  });
+}
+
+console.log(panels);

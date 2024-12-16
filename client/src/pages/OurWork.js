b5281eb00d4243034b9b0ea50ce5f906cf66374b
Paul Garton <paulgarton1984@hotmail.com>
2024-12-16 17:59:34 +0000

    import data to ourWork page and render basic layout of images

--- client/src/pages/OurWork.js
+++ client/src/pages/OurWork.js
@@ -1,3 +1,6 @@
+import "../assets/css/ourWork.css";
+import data from "../assets/data/work.json";
+
 function OurWork() {
   return (
     <div className="ourWorkPage">
@@ -6,7 +9,21 @@ function OurWork() {
           Our Wo<span>r</span>k
         </h2>
       </div>
-      <p>This is where our work will go</p>
+      <div className="galleryCardContainer">
+        {data.map((project, index) => (
+          <div className="galleryCard" key={project.id}>
+            <img
+              src={project.mainImage}
+              alt={project.imageAlt}
+              loading="lazy"
+            />
+            <div className="galleryCardInfo">
+              <h3>{project.title}</h3>
+              <p>{project.shortDescription}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
